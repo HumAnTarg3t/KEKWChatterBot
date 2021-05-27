92891a9a49171388ebae6f9077d575c422ae62a0
N.Tsa <24270865+HumAnTarg3t@users.noreply.github.com>
2021-05-27 17:28:59 +0200

    Looking after correct emote so that it can answer

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-MediaStreamAudioDestinationNode
+const tmi = require("tmi.js");
+require("dotenv").config();
+
+const client = new tmi.Client({
+  options: { debug: true },
+  identity: {
+    username: process.env.userName,
+    password: process.env.passWord,
+  },
+  // channels: ["humantarg3t"],
+  channels: ["humantarg3t"],
+});
+
+client.connect();
+let pauseOn = true;
+client.on("message", (channel, tags, message, self) => {
+  // Ignore echoed messages.
+  if (self) return;
+  if (message === "forsenE" && pauseOn) {
+    client.say(channel, "Her kommer det en historie");
+    pauseOn = false;
+  }
+  if (message === "LUL") {
+    pauseOn = true;
+  }
+});
